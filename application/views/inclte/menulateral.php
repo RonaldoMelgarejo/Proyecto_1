58dad4a96397f1f1a6773ad2c2b97439ab4a7b71
Ronaldo <melgarejo.ronaldo.590@gmail.com>
2023-09-12 10:35:46 -0400

    Cambios en Menu Lateral

--- application/views/inclte/menulateral.php
+++ application/views/inclte/menulateral.php
@@ -11,7 +11,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?php echo base_url();?>adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <!-- <img src="<?php echo base_url();?>adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">-->
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -35,6 +35,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+          <!--
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -63,7 +64,8 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>-->
+          <!--
           <li class="nav-item">
             <a href="../widgets.html" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
@@ -132,7 +134,8 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>-->
+          <!--
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-chart-pie"></i>
@@ -167,7 +170,8 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>-->
+          <!--
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tree"></i>
@@ -226,7 +230,8 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>-->
+          <!--
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-edit"></i>
@@ -261,7 +266,8 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li>-->
+          
           <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-table"></i>
@@ -291,6 +297,7 @@
               </li>
             </ul>
           </li>
+          <!--
           <li class="nav-header">EXAMPLES</li>
           <li class="nav-item">
             <a href="../calendar.html" class="nav-link">
@@ -346,6 +353,8 @@
               </li>
             </ul>
           </li>
+          -->
+          <!--
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-book"></i>
@@ -581,7 +590,8 @@
               <i class="nav-icon fas fa-file"></i>
               <p>Documentation</p>
             </a>
-          </li>
+          </li>-->
+          <!--
           <li class="nav-header">MULTI LEVEL EXAMPLE</li>
           <li class="nav-item">
             <a href="#" class="nav-link">
@@ -666,6 +676,7 @@
               <p>Informational</p>
             </a>
           </li>
+          -->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
